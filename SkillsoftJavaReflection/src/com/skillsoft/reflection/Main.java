e405570d69597c526ea791df207996527fd56279
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-31 17:31:36 -0600

    Creating Classes and Instantiating Objects

--- SkillsoftJavaReflection/src/com/skillsoft/reflection/Main.java
+++ SkillsoftJavaReflection/src/com/skillsoft/reflection/Main.java
@@ -3,5 +3,13 @@ package com.skillsoft.reflection;
 public class Main {
     public static void main(String[] args) {
 
+        Employee jack = new Employee();
+
+        System.out.println("Employee Jack: " + jack);
+
+        Employee jessica = new Employee("Jessica", "VP", 124000);
+
+        System.out.println("Employee Jessica: " + jessica);
+
     }
 }
