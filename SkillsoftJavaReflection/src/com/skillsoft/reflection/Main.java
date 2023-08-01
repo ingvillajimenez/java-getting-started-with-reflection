b176091daf85eb309b76b36dc1382e58447a9832
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-31 21:38:02 -0600

    Viewing Class Fields, Methods, and Their Modifiers

--- SkillsoftJavaReflection/src/com/skillsoft/reflection/Main.java
+++ SkillsoftJavaReflection/src/com/skillsoft/reflection/Main.java
@@ -1,38 +1,40 @@
 package com.skillsoft.reflection;
 
+import java.lang.reflect.Constructor; // Reflection Constructor class
+//import java.lang.reflect.Method; // Reflection Method class
+//import java.lang.reflect.Field; // Reflection Field class
+
 public class Main {
     public static void main(String[] args) {
 
         Employee jessica = new Employee("Jessica", "VP", 124000);
 
         System.out.println("Employee Jessica: " + jessica);
-        System.out.println("Class for object Jessica: " + jessica.getClass());
-        System.out.println("The Employee class: " + Employee.class);
+
+        Class<?> employeeClass = jessica.getClass();
+
+        System.out.println("Class for object Jessica: " + employeeClass);
         System.out.println();
 
-        System.out.println("The class object is of type java.lang.Class: " +
-                (jessica.getClass() instanceof java.lang.Class));
-        System.out.println("This is the same class object of type java.lang.Class: " +
-                (Employee.class instanceof java.lang.Class));
-        System.out.println();
+        Constructor<?>[] constructors = employeeClass.getConstructors(); // getConstructors only gets the public constructor methods
 
-        System.out.println("Both are exactly the same class objects: " +
-                (jessica.getClass().equals(Employee.class)));
-        System.out.println();
+        for (Constructor<?> constructor : constructors) {
+            System.out.println(constructor);
+        }
 
-//        System.out.println("Employee Jessica: " + jessica);
-//        System.out.println("ID: " + jessica.getEmployeeId());
-//        System.out.println("Name: " + jessica.getName());
-//        System.out.println("Title: " + jessica.getTitle());
-//        System.out.println("Salary: " + jessica.getSalary());
-
-//        Employee jack = new Employee();
+//        Method[] methods = employeeClass.getMethods(); // getMethods only gets the public methods
 //
-//        System.out.println("Employee Jack: " + jack);
-//
-//        Employee jessica = new Employee("Jessica", "VP", 124000);
-//
-//        System.out.println("Employee Jessica: " + jessica);
+//        for (Method method : methods) {
+//            System.out.println(method);
+//        }
 
+//        Field[] fields = employeeClass.getFields(); // getFields method is only going to return all of the public fields
+//
+//        System.out.println("Fields of the Employee class: " + fields);
+//        System.out.println();
+//
+//        for (Field field: fields) {
+//            System.out.println(field);
+//        }
     }
 }
