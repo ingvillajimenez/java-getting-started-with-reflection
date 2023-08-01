8f513fe31bbf25c29bda89ed29f537145b525f3f
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-01 17:37:11 -0600

    Viewing Modifiers of Class Members

--- SkillsoftJavaReflection/src/com/skillsoft/reflection/Main.java
+++ SkillsoftJavaReflection/src/com/skillsoft/reflection/Main.java
@@ -1,8 +1,10 @@
 package com.skillsoft.reflection;
 
+import java.lang.reflect.Modifier;
+
 public class Main {
 
-    private static class HR extends Department {
+    private static final class HR extends Department {
 
         public HR() {
             super("HR");
@@ -16,28 +18,48 @@ public class Main {
         Class<?> hrClass = Class.forName("com.skillsoft.reflection.Main$HR");
         Class<?> departmentClass = Class.forName("com.skillsoft.reflection.Department");
 
-        System.out.println("****** toString() representations of custom class objects");
-        System.out.println("Organization: " + organizationClass); // class com.skillsoft.reflection.Organization
-        System.out.println("Employee: " + employeeClass); // class com.skillsoft.reflection.Employee
-        System.out.println("HR: " + hrClass); // class com.skillsoft.reflection.Main$HR
-        System.out.println("Department: " + departmentClass); // class com.skillsoft.reflection.Department
+        System.out.println("****************** Organization");
 
-        System.out.println();
+        int organizationModifiers = organizationClass.getModifiers();
+        System.out.println("Organization class modifiers: " + organizationModifiers); // 17
+        System.out.println("Organization class modifiers in binary format: " +
+                Integer.toBinaryString(organizationModifiers)); // 10001
 
-        Class<?> doubleClass = Class.forName("java.lang.Double");
-        Class<?> hashSetClass = Class.forName("java.util.HashSet");
-        Class<?> stringArrayClass = Class.forName("[[Ljava.lang.String;");
-        Class<?> intArrayClass = Class.forName("[I");
+        System.out.println("Public: " + Modifier.isPublic(organizationModifiers)); // true
+        System.out.println("Final: " + Modifier.isFinal(organizationModifiers)); // true
 
-        System.out.println("****** toString() representations of Java language class objects");
-        System.out.println("Double: " + doubleClass); // class java.lang.Double
-        System.out.println("HashSet: " + hashSetClass); // class java.util.HashSet
-        System.out.println("String array: " + stringArrayClass); // class [[Ljava.lang.String;
-        System.out.println("int array: " + intArrayClass); // class [I
+        System.out.println("Abstract: " + Modifier.isAbstract(organizationModifiers)); // false
+        System.out.println("Static: " + Modifier.isStatic(organizationModifiers)); // false
 
-        System.out.println();
+        System.out.println("****************** Department");
 
-        Class<?> enggClass = Class.forName("com.skillsoft.reflection.Main$Engineering"); // java.lang.ClassNotFoundException
+        int departmentModifiers = departmentClass.getModifiers();
+        System.out.println("Department class modifiers: " + departmentModifiers); // 1025
+        System.out.println("Department class modifiers in binary format: " +
+                Integer.toBinaryString(departmentModifiers)); // 10000000001
+
+        System.out.println("Public: " + Modifier.isPublic(departmentModifiers)); // true
+        System.out.println("Abstract: " + Modifier.isAbstract(departmentModifiers)); // true
+
+        System.out.println("******************* HR");
+
+        int hrModifiers = hrClass.getModifiers();
+        System.out.println("HR class modifiers: " + hrModifiers); // 26
+        System.out.println("HR class modifiers in binary format: " +
+                Integer.toBinaryString(hrModifiers)); // 11010
+
+        System.out.println("Private: " + Modifier.isPrivate(hrModifiers)); // true
+        System.out.println("Static: " + Modifier.isStatic(hrModifiers)); // true
+        System.out.println("Final: " + Modifier.isFinal(hrModifiers)); // true
+
+        System.out.println("******************* Employee");
+
+        int employeeModifiers = employeeClass.getModifiers();
+        System.out.println("Employee class modifiers: " + employeeModifiers); // 1
+        System.out.println("Employee class modifiers in binary format: " +
+                Integer.toBinaryString(employeeModifiers)); // 1
+
+        System.out.println("Public: " + Modifier.isPublic(employeeModifiers)); // true
     }
 }
 
